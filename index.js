1b7ab222b7a9e0d4a85d04e0d83a99b05015c57e
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-07-19 00:14:20 +0800

    started routes

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// rooutes / url / endpoint utama dari method  GET
+app.get("/", (req, res) => {
+  res.send("utama");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("haaloo welcome rafi");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
